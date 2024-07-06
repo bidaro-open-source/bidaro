531d68d4f6e89e7484485c5ebaaf43e979b635d9
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2024-07-07 02:35:46 +0300

    ref: move redis session to server utils #6

--- server/utils/use-redis.ts
+++ server/utils/use-redis.ts
@@ -1,12 +1,31 @@
+import type { H3Event } from 'h3'
 import { Redis } from 'ioredis'
 
-export default function () {
-  const runtimeConfig = useRuntimeConfig()
+let instance: Redis | undefined
 
-  return new Redis({
-    host: runtimeConfig.redis.host,
-    port: +runtimeConfig.redis.port,
-    username: runtimeConfig.redis.user,
-    password: runtimeConfig.redis.pass,
-  })
+/**
+ * Returns Redis instance.
+ *
+ * @param event H3Event
+ * @returns redis instance
+ * @trhows if connection is not success
+ */
+export default function (event?: H3Event) {
+  try {
+    const runtimeConfig = useRuntimeConfig(event)
+
+    if (!instance) {
+      instance = new Redis({
+        host: runtimeConfig.redis.host,
+        port: +runtimeConfig.redis.port,
+        username: runtimeConfig.redis.user,
+        password: runtimeConfig.redis.pass,
+      })
+    }
+
+    return instance
+  }
+  catch (e) {
+    throw new Error('Redis is not connected.')
+  }
 }
