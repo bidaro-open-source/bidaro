4b2e28b2a43eb0895134f734e69c43cb75209d79
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2024-07-10 00:17:45 +0300

    ref: add comments to server utils #6

--- server/utils/json-web-token.ts
+++ server/utils/json-web-token.ts
@@ -6,6 +6,12 @@ export interface JsonWebTokenPayload {
   uid: number
 }
 
+/**
+ * Returns signed access token.
+ *
+ * @param payload token data
+ * @returns json web token
+ */
 export function createAccessToken(payload: JsonWebTokenPayload): JsonWebToken {
   const runtimeConfig = useRuntimeConfig()
 
@@ -16,6 +22,12 @@ export function createAccessToken(payload: JsonWebTokenPayload): JsonWebToken {
   )
 }
 
+/**
+ * Returns token verifed status.
+ *
+ * @param token json web token
+ * @returns token verifed or not
+ */
 export function verifyAccessToken(token: JsonWebToken): boolean {
   try {
     jwt.verify(token, useRuntimeConfig().jwt.secret)
@@ -26,6 +38,12 @@ export function verifyAccessToken(token: JsonWebToken): boolean {
   }
 }
 
+/**
+ * Returns token payload without verification.
+ *
+ * @param token json web token
+ * @returns token data
+ */
 export function decodeAccessToken(token: JsonWebToken): JsonWebTokenPayload {
   return jwt.decode(token) as JsonWebTokenPayload
 }
