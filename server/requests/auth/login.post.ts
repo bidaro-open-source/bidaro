fedb30069e17b0ebedaccbd170b22cd0f107f7a2
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2024-07-15 14:18:04 +0300

    ref: update User model types and length of columns #6

--- server/requests/auth/login.post.ts
+++ server/requests/auth/login.post.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod'
 
 export const schema = z.object({
-  username: z.string().min(1).max(24).regex(/^(?=.*[a-z])\w+$/i),
-  password: z.string().min(1).max(64),
+  username: z.string().trim().min(2).max(24).regex(/^(?=.*[a-z])\w+$/i),
+  password: z.string().trim().min(1).max(64),
 })
 
 export type RequestBody = z.infer<typeof schema>
