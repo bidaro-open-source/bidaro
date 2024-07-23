62924753099d19fcb953e825eee7f291885ae103
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2024-07-23 12:21:42 +0300

    ref: rewrite request parsing to validating in sessions api #14

--- server/api/user/[uid]/sessions.get.ts
+++ server/api/user/[uid]/sessions.get.ts
@@ -1,19 +1,19 @@
-import { parseRequest } from '~/server/requests/user/sessions.get'
+import { getSessionsRequest } from '~/server/requests/user/sessions'
 import { getAuthenticationSessions } from '~/server/services/authentication'
 
 export default defineEventHandler(async (event) => {
   mustBeAuthenticated(event)
 
-  const params = await parseRequest(event)
+  const request = await validateRequest(event, getSessionsRequest)
 
-  if (event.context.auth.uid !== params.uid) {
+  if (event.context.auth.uid !== request.params.uid) {
     throw createError({
       statusCode: 403,
       message: 'Немає доступу',
     })
   }
 
-  const sessions = await getAuthenticationSessions(params.uid)
+  const sessions = await getAuthenticationSessions(request.params.uid)
 
   return Object.values(sessions)
 })
