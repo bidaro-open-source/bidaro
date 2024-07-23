1e612436f4a22471e935201eb3cb06456ef7681b
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2024-07-23 12:21:42 +0300

    ref: rewrite request parsing to validating in sessions api #14

--- server/api/user/[uid]/sessions.delete.ts
+++ server/api/user/[uid]/sessions.delete.ts
@@ -1,17 +1,20 @@
-import { parseRequest } from '~/server/requests/user/sessions.delete'
+import { deleteSessionsRequest } from '~/server/requests/user/sessions'
 import { deleteAuthenticationSessions } from '~/server/services/authentication'
 
 export default defineEventHandler(async (event) => {
   mustBeAuthenticated(event)
 
-  const { body, params } = await parseRequest(event)
+  const request = await validateRequest(event, deleteSessionsRequest)
 
-  if (event.context.auth.uid !== params.uid) {
+  if (event.context.auth.uid !== request.params.uid) {
     throw createError({
       statusCode: 403,
       message: 'Немає доступу',
     })
   }
 
-  return await deleteAuthenticationSessions(params.uid, body.uuids)
+  return await deleteAuthenticationSessions(
+    request.params.uid,
+    request.body.uuids,
+  )
 })
