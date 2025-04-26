7225b2cc5021e9ee5bced146f40617b48f3f469e
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-04-26 23:37:25 +0300

    feat: add profile root endpoint

---
+++ server/api/profile/index.get.ts
@@ -0,0 +1,33 @@
+export default defineEventHandler(async (event) => {
+  const user = mustBeAuthenticated(event)
+
+  const db = useDatabase(event)
+
+  const userInDB = await db.User.findOne({
+    where: { id: user.id },
+    include: [
+      {
+        model: db.Role,
+        as: 'role',
+        attributes: ['name'],
+        include: [
+          {
+            model: db.Permission,
+            as: 'permissions',
+            attributes: ['name'],
+          },
+        ],
+      },
+    ],
+  })
+
+  if (!userInDB) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Not Found',
+      message: 'Користувач не існує',
+    })
+  }
+
+  return userInDB
+})
