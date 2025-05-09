7694e9d4b473a788ea55d6368b36558e17b433de
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-05-09 21:49:12 +0300

    feat: add intro to home page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div></div>
+  <main>
+    <section class="pt-32">
+      <div class="mx-auto container px-4">
+        <div class="w-full max-w-lg">
+          <h1 class="text-4xl font-bold text-black dark:text-white w-md">
+            Ласкаво просимо до Bidaro – вашого аукціону!
+          </h1>
+          <p class="mt-4">
+            Bidaro об'єднує покупців і продавців, створюючи активну спільноту аукціонів. Долучайтеся до нас, щоб знайти унікальні товари та зробити вигідні угоди.
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
