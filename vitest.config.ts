96ff19b8b18acd87b6b91ad3da8609d44aea7f52
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-06-10 04:10:23 +0300

    test: update include value

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   root: './',
   test: {
+    include: ['test/**/*.test.ts'],
     passWithNoTests: true,
     globalSetup: 'test/setup.ts',
     setupFiles: [
