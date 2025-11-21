6edbe4a5a3245c63064046e9f8be733af30f9105
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-11-21 22:49:56 +0200

    ref: change database transation to managed

--- server/utils/use-database-transaction.ts
+++ server/utils/use-database-transaction.ts
@@ -1,9 +1,25 @@
+import type { Transaction } from 'sequelize'
+
 /**
- * Returns a Sequelize transaction instance.
+ * Creates a database transaction.
  *
- * @param event H3Event
  * @returns A Sequelize transaction instance
  */
-export function useDatabaseTransaction(event?: H3Event) {
-  return useDatabase(event).sequelize.transaction()
+export function useDatabaseTransaction(): Promise<Transaction>
+
+/**
+ * Creates a database transaction with callback.
+ *
+ * @param callback Callback function that receives the transaction instance.
+ * @returns result of the callback function.
+ */
+export function useDatabaseTransaction<T>(callback: (t: Transaction) => PromiseLike<T>): Promise<T>
+export function useDatabaseTransaction<T>(callback?: (t: Transaction) => PromiseLike<T>): Promise<T | Transaction> {
+  const db = useDatabase()
+
+  if (callback) {
+    return db.sequelize.transaction(callback)
+  }
+
+  return db.sequelize.transaction()
 }
