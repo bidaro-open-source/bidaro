69826ce219b9157d129a505b41c0380bd67d04b0
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-11-21 23:47:09 +0200

    feat: add send emails by closing lot

--- server/api/lots/[id]/close/index.post.ts
+++ server/api/lots/[id]/close/index.post.ts
@@ -1,3 +1,4 @@
+import { userRepository } from '~~/server/repositories/user.repository'
 import { createLotResource } from '~~/server/resources/lot.resource'
 import { lotService } from '~~/server/services/lot.service'
 import { getLotRequest } from '../index.request'
@@ -13,6 +14,29 @@ export default defineEventHandler(async (event) => {
   closeLotPolicy(event, lot)
 
   const updatedLot = await lotService.close(request.params.id)
+
+  if (updatedLot.winnerId) {
+    const winner = await userRepository.findById(updatedLot.winnerId)
+    const seller = await userRepository.findById(updatedLot.sellerId)
+
+    winner && await sendEmail(event, {
+      to: winner.email,
+      subject: 'Вітаємо! Ви виграли лот на Bidaro',
+      template: {
+        html: `Вітаємо! Ви виграли лот "${updatedLot.title}". Зв'яжіться з продавцем: ${seller?.email}`,
+        text: `Вітаємо! Ви виграли лот "${updatedLot.title}". Зв'яжіться з продавцем: ${seller?.email}`,
+      },
+    })
+
+    seller && await sendEmail(event, {
+      to: seller.email,
+      subject: 'Ваш лот було продано на Bidaro',
+      template: {
+        html: `Ваш лот "${updatedLot.title}" було продано користувачу: ${winner?.email}`,
+        text: `Ваш лот "${updatedLot.title}" було продано користувачу: ${winner?.email}`,
+      },
+    })
+  }
 
   return createLotResource(updatedLot)
 })
