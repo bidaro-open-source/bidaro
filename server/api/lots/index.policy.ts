355e72e5f3074307be5d4936a6e4c477f371a6e9
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-11-25 23:23:13 +0200

    feat: add pagination to get lots api

---
+++ server/api/lots/index.policy.ts
@@ -0,0 +1,8 @@
+import { permissions } from '~~/server/constants'
+
+export const viewLotsPolicy = createRequestPolicy((event: H3Event) => {
+  const userPermissions = getAuthenticatedUserPermissions(event)
+  if (!userPermissions)
+    return false
+  return hasPermission(userPermissions, permissions.VIEW_LOTS)
+})
