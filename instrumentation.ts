e13cf9d33e20788ca195ab36a6edc6f5da44e653
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-12-03 22:56:22 +0200

    feat: add logger with open telemetry itegration

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,14 +1,21 @@
 import process from 'node:process'
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-grpc'
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
 import { AwsInstrumentation } from '@opentelemetry/instrumentation-aws-sdk'
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis'
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg'
+import { WinstonInstrumentation } from '@opentelemetry/instrumentation-winston'
 import { resourceFromAttributes } from '@opentelemetry/resources'
+import { BatchLogRecordProcessor } from '@opentelemetry/sdk-logs'
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
 import { NodeSDK } from '@opentelemetry/sdk-node'
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions'
+
+const logExporter = new OTLPLogExporter({
+  url: 'http://localhost:4317',
+})
 
 const traceExporter = new OTLPTraceExporter({
   url: 'http://localhost:4317',
@@ -24,6 +31,7 @@ const sdk = new NodeSDK({
     [ATTR_SERVICE_VERSION]: '1.0.0',
   }),
   traceExporter,
+  logRecordProcessor: new BatchLogRecordProcessor(logExporter),
   metricReader: new PeriodicExportingMetricReader({
     exporter: metricExporter,
     exportIntervalMillis: 1000,
@@ -31,6 +39,17 @@ const sdk = new NodeSDK({
   instrumentations: [
     new HttpInstrumentation(),
     new PgInstrumentation(),
+    new WinstonInstrumentation({
+      logHook: (span, record) => {
+        const context = span.spanContext()
+
+        record.message = {
+          body: record.message,
+          trace_id: context.traceId,
+          span_id: context.spanId,
+        }
+      },
+    }),
     new IORedisInstrumentation({
       requireParentSpan: false,
     }),
---
+++ server/utils/logger.ts
@@ -0,0 +1,31 @@
+import winston from 'winston'
+
+const consoleFormat = winston.format.printf(({ level, message, timestamp, trace_id }) => {
+  let logBody = message
+
+  if (typeof message === 'object' && message !== null) {
+    const cleanObj = { ...message }
+    delete (cleanObj as any).trace_id
+    delete (cleanObj as any).span_id
+
+    logBody = JSON.stringify(cleanObj)
+  }
+
+  return `${timestamp} [${level}]: ${logBody} ${trace_id ? `trace_id=${trace_id}` : ''}`
+})
+
+export const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        consoleFormat,
+      ),
+    }),
+  ],
+})
