b37dab15574b8e42b090ef8606bf592793a591f7
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-12-11 02:54:59 +0200

    test: update diagnostic test

--- test/api-e2e/api/diagnostic.get.test.ts
+++ test/api-e2e/api/diagnostic.get.test.ts
@@ -1,22 +1,38 @@
 import { env } from 'node:process'
 import { describe, expect, it } from 'vitest'
 import { fetch } from '~~/test/api-e2e/fetch'
+import { expectApiError } from '../utils/expect-api-error'
 
-async function diagnosticRequest() {
+async function diagnosticRequest(headers = {}) {
   return await fetch(`/api/diagnostic`, {
     method: 'GET',
-    headers: {
-      'x-diagnostic-token': env.NUXT_DIAGNOSTIC_TOKEN || '',
-    },
+    headers,
   })
 }
 
 describe('GET /api/diagnostic', async () => {
   it('should return ok', async () => {
-    const response = await diagnosticRequest()
+    const response = await diagnosticRequest({
+      'x-diagnostic-token': env.DIAGNOSTIC_TOKEN || '',
+    })
 
     const data = response._data
 
+    expect(response.status).toBe(200)
     expect(data.ok).toBe(true)
+  })
+
+  it('should return 401 when token not exist', async () => {
+    const response = await diagnosticRequest()
+
+    expectApiError(response, 'UNAUTHORIZED')
+  })
+
+  it('should return 401 when token invalid', async () => {
+    const response = await diagnosticRequest({
+      'x-diagnostic-token': 'invalid-token',
+    })
+
+    expectApiError(response, 'UNAUTHORIZED')
   })
 })
