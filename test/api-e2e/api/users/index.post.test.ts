630f2603eef9452f59ae75c41d8058e086261025
Dmitry Tavern <dmytrotraktirnyi@gmail.com>
2025-12-11 02:21:20 +0200

    test: remove duplicate destroyUser function

--- test/api-e2e/api/users/index.post.test.ts
+++ test/api-e2e/api/users/index.post.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, it } from 'vitest'
 import { permissions } from '~~/server/constants'
 import { createUser } from '~~/test/api-e2e/arrangers/create-user'
 import { fetch } from '~~/test/api-e2e/fetch'
+import { destroyUser } from '../../requests/authentication'
 import { expectApiError } from '../../utils/expect-api-error'
 
 async function createUserRequest(
@@ -14,10 +15,6 @@ async function createUserRequest(
     body: payload.body,
     accessToken: options.accessToken,
   })
-}
-
-async function destroyUser(id: number) {
-  return await db.User.destroy({ where: { id } })
 }
 
 describe('POST /api/users', async () => {
